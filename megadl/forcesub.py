77bc9f0468682f483378b3d7df30786b7aac54b2
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2021-10-09 00:09:52 +0530

    Update forcesub.py

--- megadl/forcesub.py
+++ megadl/forcesub.py
@@ -19,7 +19,7 @@ async def handle_force_subscribe(bot, message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=message.from_user.id,
-                text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/safothebot).",
+                text="Sorry bro, You are Banned. Contact My [Support Group](https://t.me/AJPyroVerseGroup).",
                 parse_mode="markdown",
                 disable_web_page_preview=True,
                 reply_to_message_id=message.message_id,
@@ -28,14 +28,14 @@ async def handle_force_subscribe(bot, message):
     except UserNotParticipant:
         await bot.send_message(
             chat_id=message.from_user.id,
-            text="**Please Join My Updates Channel To Use Me!**\n\nDue to Overload, Only Channel Subscribers Can Use Me!",
+            text="**Please Join My Channel in order to use me!**\n\nDue to Overload, Only my Channel Subscribers Can Use Me!",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🤖 Join Updates Channel 🤖", url=invite_link.invite_link)
+                        InlineKeyboardButton("Join Updates Channel", url=invite_link.invite_link)
                     ],
                     [
-                        InlineKeyboardButton("🔄 Refresh 🔄", callback_data="refreshmeh")
+                        InlineKeyboardButton("Refresh", callback_data="refreshmeh")
                     ]
                 ]
             ),
@@ -46,7 +46,7 @@ async def handle_force_subscribe(bot, message):
     except Exception:
         await bot.send_message(
             chat_id=message.from_user.id,
-            text="Something Went Wrong. Contact My [Support Group](https://t.me/safothebot).",
+            text="Something Went Wrong. Contact My [Support Group](https://t.me/AJPyroVerseGroup).",
             parse_mode="markdown",
             disable_web_page_preview=True,
             reply_to_message_id=message.message_id,
