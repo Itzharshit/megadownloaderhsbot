8de60ffb55a38489e3ed994ee1dc6dec7cb497e7
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2021-10-09 10:24:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,24 +16,11 @@ class Config:
 
 
 class TEXT:
-  ABOUT = """
-**Name:** {bot_name}
-
-**Language:** [Python](https://www.python.org)
-
-**Library:** [Pyrogram](https://docs.pyrogram.org)
-
-**Hosted On:** [Heroku](https://heroku.com)
-
-**Developer:** [Safone](https://t.me/ImSafone)
-
-**Support Group:** [SafoTheBot](https://t.me/safothebot)
-
-**Updates Channel:** [Ｓ１ ＢＯＴＳ](https://t.me/AsmSafone)
+  ABOUT = """ Sorry can't hear you.
 """
 
   HELP_USER = """
-I am Mega Downloader and captions adder bot
+I am Mega Downloader and captions adder bot.
 
 I can download files from mega.nz, Send me any link of mega.nz file and i will download it for you.I can also add caption for your file. You have to only reply your file with caption text, i will automatically add captions! 
 """
@@ -41,6 +28,6 @@ I can download files from mega.nz, Send me any link of mega.nz file and i will d
   START_TEXT = """
 Hi {user_mention},
 
-I'm Mega Downloader bot
+I'm Mega Downloader bot.
 I can download files from mega.nz right in your Telegram.
 """
