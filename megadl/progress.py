4854153b3caaa469b50adee75e6034ef96a22715
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2021-10-09 10:48:37 +0530

    Update progress.py

--- megadl/progress.py
+++ megadl/progress.py
@@ -30,15 +30,15 @@ async def progress_for_pyrogram(
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
         progress = "`[{0}{1}]` \n".format(
-            ''.join(["■" for i in range(math.floor(percentage / 5))]),
-            ''.join(["□" for i in range(20 - math.floor(percentage / 5))])
+            ''.join(["●" for i in range(math.floor(percentage / 5))]),
+            ''.join(["○" for i in range(20 - math.floor(percentage / 5))])
             )
         
         ok = "`{0}%` \n".format(
             round(percentage, 2)
             )
 
-        tmp = ok + progress + "\n**Uploaded:** `{0}` \n**File Size:** `{1}` \n**Speed:** `{2}/s` \n**Time Left:** `{3}`".format(
+        tmp = ok + progress + "\n👉 **Done:** `{0}` \n👉 **Total:** `{1}` \n👉 **Speed:** `{2}/s` \n👉 **Time Left:** `{3}`".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
@@ -46,7 +46,7 @@ async def progress_for_pyrogram(
         )
         try:
             await message.edit(
-                text="**{}** {} \n\n****".format(
+                text="**{}** {} \n\n**Thanks for using our bot.**".format(
                     ud_type,
                     tmp
                 ),
